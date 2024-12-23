f95747d7e0bbfa2ca7b7d0c8d6b707c72b976741
Mevvi3 <vv.miotke@gmail.com>
2024-12-23 23:46:57 +0100

    SCRUM-18 - added: customers, customers/forms.py - app setup, model, customer form

--- backend/customers/models.py
+++ backend/customers/models.py
@@ -1,31 +1,22 @@
 from django.db import models
-from django.contrib.auth.hashers import make_password
 
 
 class Customer(models.Model):
-    class Gender(models.TextChoices):
-        MALE = 'M', 'Male'
-        FEMALE = 'F', 'Female'
-        OTHER = 'O', 'Other'
+    class Genders(models.TextChoices):
+        Male = 'Male'
+        Female = 'Female'
+        Other = 'Other'
 
-    customerID = models.CharField(max_length=100, primary_key=True) # login
-    password = models.CharField(max_length=100)
-
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    gender = models.CharField(max_length=1, choices=Gender.choices, default=Gender.OTHER)
+    name = models.CharField(max_length=100, blank=True)
+    surname = models.CharField(max_length=100 , blank=True)
+    gender = models.CharField(max_length=10, choices=Genders, default=Genders.Other)
     email = models.EmailField()
-    phone = models.CharField(max_length=9) # we assume that the phone number is 9 digits (like in Poland)
-    address = models.CharField(max_length=1000)
+    phone = models.CharField(max_length=9, blank=True) # we assume that the phone number is 9 digits (like in Poland)
+    address = models.CharField(max_length=1000, blank=True)
     preferences = models.JSONField(null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    def save(self, *args, **kwargs):
-        if not self.pk or 'password' in self.get_dirty_fields():
-            self.password = make_password(self.password)
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
