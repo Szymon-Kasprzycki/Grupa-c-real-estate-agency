6a5f6252ed6d0b38d38a4e4fd7838ebc3d9efd09
Mevvi3 <vv.miotke@gmail.com>
2024-12-26 14:57:20 +0100

    SCRUM-22 - changed: backend/urls.py - added customers path, added: customers/urls.py, templates/home.html, templates/login.html, templates/register.html - added paths for simple frontend for login, register and home page

--- backend/customers/urls.py
+++ backend/customers/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 from .views import home, login_page, register_page
 
 urlpatterns = [
-    path('home/', home),
-    path('login/', login_page),
-    path('register/', register_page), 
+    path('home/', home, name='home_page'),
+    path('login/', login_page, name='login_page'),
+    path('register/', register_page, name='register_page'), 
 ]
