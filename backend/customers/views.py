6225f9c0136269d359ebffcafb234d291ac7f36f
Mevvi3 <vv.miotke@gmail.com>
2024-12-26 14:59:00 +0100

    SCRUM-22 - changed: customers/forms.py, customers/views.py - autogenerated customerRegisterform, fixed register view

--- backend/customers/views.py
+++ backend/customers/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login
-from .models import Customer
+from .forms import CustomerRegisterForm
 
 
 def home(request):
@@ -16,48 +16,46 @@ def login_page(request):
         
         if not User.objects.filter(username=username).exists():
             messages.error(request, 'Invalid Username')
-            return redirect('login/')
+            return redirect('/customers/login/')
         
         user = authenticate(username=username, password=password)
         
         if user is None:
             messages.error(request, "Invalid Password")
-            return redirect('login/')
+            return redirect('/customers/login/')
         else:
             login(request, user)
-            return redirect('home/')
+            return redirect('/customers/home/')
     
     return render(request, 'login.html')
 
 def register_page(request):
     if request.method == 'POST':
+
         username = request.POST.get('username')
         password = request.POST.get('password')
 
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        email = request.POST.get('email')
-        phone_number = request.POST.get('phone_number')
-        
-        user = User.objects.filter(username=username)
-        
-        if user.exists():
-            messages.info(request, "Username already in use.")
-            return redirect('register/')
-        
-        user = User.objects.create_user(username=username)
-        user.set_password(password)
-        user.save()
+        form = CustomerRegisterForm(request.POST)
+        if form.is_valid():
 
-        customer = Customer.objects.create(
-            user=user, 
-            first_name=first_name, 
-            last_name=last_name, 
-            email=email,
-            phone_number=phone_number
-            )
+            user = User.objects.filter(username=username)
+            if user.exists():
+                messages.info(request, "Username already in use.")
+                return redirect('/customers/register/')
 
-        messages.info(request, "Account created Successfully.")
-        return redirect('register/')
+            user = User.objects.create_user(username=username)
+            user.set_password(password)
+            user.save()
+
+            customer = form.save(commit=False)
+            
+            customer.user = user
+            customer.save()
+            
+            messages.info(request, "Account created Successfully.")
+            return redirect('/customers/home/')
+    else:
+        form = CustomerRegisterForm()
     
-    return render(request, 'register.html')
+    return render(request, 'register.html', {'form': form})
+
