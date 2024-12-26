0a3597b772c2814fcad86c8cc2b1aab258b270a8
Mevvi3 <vv.miotke@gmail.com>
2024-12-26 14:53:46 +0100

    SCRUM-22 - changed: customers/models.py - constraints on phone number and email

--- backend/customers/models.py
+++ backend/customers/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator, MaxLengthValidator, RegexValidator
 
 
 class Customer(models.Model):
@@ -8,13 +9,18 @@ class Customer(models.Model):
         Female = 'Female'
         Other = 'Other'
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
 
     first_name = models.CharField(max_length=100, blank=True)
     last_name = models.CharField(max_length=100 , blank=True)
     gender = models.CharField(max_length=10, choices=Genders, default=Genders.Other)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=9, blank=True) # we assume that the phone number is 9 digits (like in Poland)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=9,
+                                    validators=[MinLengthValidator(9), 
+                                                MaxLengthValidator(9),
+                                                RegexValidator(regex='^\d{9}$', message='Phone number must be 9 digits long and contain only numbers.')],
+                                    blank=True,
+                                    unique=True)
     address = models.CharField(max_length=1000, blank=True)
     preferences = models.JSONField(null=True, blank=True)
 
@@ -22,4 +28,4 @@ class Customer(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.first_name
