81fda0c8ec54bc36e386c8bdcd3a3c4e78e77b7d
Szymon Kasprzycki <szymon.kasprzycki@gmail.com>
2024-12-30 23:27:35 +0100

    adjusted branch to only task - removed elements copied from other task

--- backend/properties/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views, endpoints
-
-urlpatterns = [
-    path('create/', endpoints.create_property, name='create_property'),
-    #path('', views.properties, name='properties'),
-    # path('<int:pk>/', views.property, name='property'),
-]
