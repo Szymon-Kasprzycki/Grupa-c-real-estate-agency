02ed43b1836be04a2c6a48ef7e59398165815684
Szymon Kasprzycki <szymon.kasprzycki@gmail.com>
2024-12-30 23:47:00 +0100

    removed unnecessary changes

--- backend/customers/models.py
+++ backend/customers/models.py
@@ -1,17 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator, MaxLengthValidator, RegexValidator
-from django.contrib.auth.models import AbstractUser
-from django.utils.timezone import now
 
-class User(AbstractUser):
-    username = models.CharField(max_length=150, unique=True)
-    password_hash = models.CharField(max_length=255, blank=True, null=True)
-    creation_date = models.DateTimeField(default=now, editable=False)
-    last_update_date = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.username
 
 class Customer(models.Model):
     class Genders(models.TextChoices):
