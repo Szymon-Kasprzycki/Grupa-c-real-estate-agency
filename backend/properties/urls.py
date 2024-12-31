f649152d6f3c814c869b8164c77b0b4101ec3be5
Szymon Kasprzycki <szymon.kasprzycki@gmail.com>
2024-12-31 01:30:19 +0100

    refactor

--- backend/properties/urls.py
+++ backend/properties/urls.py
@@ -3,6 +3,4 @@ from . import views, endpoints
 
 urlpatterns = [
     path('create', endpoints.create_property, name='create_property'),
-    # path('', views.properties, name='properties'),
-    # path('<int:pk>/', views.property, name='property'),
 ]
