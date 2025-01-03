4604fe7ae8ccf682759dc9c6b695a47dd197b4af
Szymon Kasprzycki <szymon.kasprzycki@gmail.com>
2025-01-03 10:57:44 +0100

    SCRUM-42 - added: properties - endpoint for property info get

--- backend/properties/endpoints.py
+++ backend/properties/endpoints.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 # from .forms import PropertyForm, PropertyImageForm
-from .serializers import PropertySerializer, PropertyImageSerializer
+from .serializers import PropertySerializer, ForListPropertyDeserializer, EntityPropertyDeserializer
+from .models import Property, PropertyImage
 
 
 def create_property(request):
@@ -15,3 +16,28 @@ def create_property(request):
             return HttpResponse(status=400, content=str(dict(message='error', errors=property_form.errors)))
     else:
         return render(request, 'base_property_form.html')
+
+
+def get_property(request, pk):
+    if request.method == 'GET':
+        try:
+            property_obj = Property.objects.get(pk=pk)
+            property_deserializer = EntityPropertyDeserializer(property_obj)
+            return HttpResponse(status=200, content=str(dict(message='success', property=property_deserializer.data)))
+        except Property.DoesNotExist:
+            return HttpResponse(status=404, content=str(dict(message='error', errors='Property not found')))
+    else:
+        return HttpResponse(status=405, content=str(dict(message='error', errors='Method not allowed')))
+
+
+def get_properties(request):
+    if request.method == 'GET':
+        properties = Property.objects.all()
+        properties_list = []
+        for property in properties:
+            property_serializer = ForListPropertyDeserializer(property)
+            if property_serializer.data:
+                properties_list.append(property_serializer.data)
+        return HttpResponse(status=200, content=str(dict(message='success', properties=properties_list)))
+    else:
+        return HttpResponse(status=405, content=str(dict(message='error', errors='Method not allowed')))
